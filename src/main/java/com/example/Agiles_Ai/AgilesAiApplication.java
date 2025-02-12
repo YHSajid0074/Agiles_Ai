ff94cfb8cd29b645e0e81c491f760af0e213c117
Sajid <yeamimhossainsajid@gmail.com>
2025-02-12 22:14:21 +0600

    first commit

---
+++ src/main/java/com/example/Agiles_Ai/AgilesAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Agiles_Ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgilesAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgilesAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Agiles_Ai/AgilesAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Agiles_Ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgilesAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
